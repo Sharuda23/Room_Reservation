4e1809e4ba9d655e808f425892d2b19c26817597
Sharuda23 <sharuda23@gmail.com>
2022-01-18 22:09:02 +0530

    first commit

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class LoginController {
+    public ImageView HomeIco;
+
+    public void ClickOnAction(ActionEvent actionEvent) {
+
+    }
+}
+

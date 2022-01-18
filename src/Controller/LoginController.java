86144461f7e8e1ea125af33aaa300ec422503a0d
Sharuda23 <sharuda23@gmail.com>
2022-01-19 04:26:33 +0530

    added login validation

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -19,18 +20,39 @@ public class LoginController {
     public AnchorPane LoginContext;
     public TextField txtUserName;
     public PasswordField pwdPassword;
-    String Admin="amin";
+
+    int atmp=0;
+
     public void ClickOnAction(ActionEvent actionEvent) throws IOException {
-        if(Admin == "admin") {
-            Stage stage =(Stage) LoginContext.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/AdminMenu.fxml"))));
+        String tempUserName=txtUserName.getText();
+        String  tempPassword=pwdPassword.getText();
+
+        atmp++;
+
+        if(atmp<=3){
+            if(tempUserName.equals("admin")&& tempPassword.equals("1234")) {
+                new Alert(Alert.AlertType.CONFIRMATION,"SuccessFully Logged In ! As an ADMIN").show();
+                Stage stage =(Stage) LoginContext.getScene().getWindow();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/AdminMenu.fxml"))));
+            }else if(tempUserName.equals("user")&& tempPassword.equals("0000")){
+                new Alert(Alert.AlertType.WARNING,"SuccessFully Logged In ! As a USER").show();
+                Stage stage =(Stage) LoginContext.getScene().getWindow();
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/ReciptionistMenu.fxml"))));
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Try Again !").show();
+            }
         }else{
-            Stage stage =(Stage) LoginContext.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/ReciptionistMenu.fxml"))));
+            new Alert(Alert.AlertType.WARNING,"You're Blocked !").show();
+            txtUserName.setEditable(false);
+            pwdPassword.setEditable(false);
         }
+
+
     }
 
-    public void ClickOnHome(MouseEvent mouseEvent) {
+    public void ClickOnHome(MouseEvent mouseEvent) throws IOException {
+        Stage stage =(Stage) LoginContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../View/Home.fxml"))));
     }
 }
 
